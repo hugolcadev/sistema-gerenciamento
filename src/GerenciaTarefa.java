afab55845482b2380988922d6fabc2e1172f9681
hugolca <hugolcabreu@gmail.com>
2025-11-19 02:13:44 -0300

    Criação da classe Main

--- src/GerenciaTarefa.java
+++ src/GerenciaTarefa.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class GerenciaTarefa {
     Scanner sc = new Scanner(System.in);
-    
+    ArrayList<Tarefa> tarefas = new ArrayList<>();
+
 }
