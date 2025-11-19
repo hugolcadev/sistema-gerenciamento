54279000a18693e47fdfd62ea8965d8d20360bea
hugolca <hugolcabreu@gmail.com>
2025-11-19 02:51:37 -0300

    Criação método CriaTarefa

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Bem vindo ao Gerenciador de Tarefas!\n\n");
+        System.out.println("1. Criar tarefa.\n");
+        System.out.println("2. Listar tarefas.\n");
+        System.out.println("3. Editar tarefa.\n");
+        System.out.println("4. Remover tarefa.\n");
+        int opcao = sc.nextInt();
+
+
+
+    }
     
 }
