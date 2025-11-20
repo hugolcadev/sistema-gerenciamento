4e2521788cb10c9f61273ed58d30e47c8230a87c
hugolca <hugolcabreu@gmail.com>
2025-11-20 00:50:08 -0300

    Criação do método exibeTarefa e modificações em listaTarefas

--- src/GerenciaTarefa.java
+++ src/GerenciaTarefa.java
@@ -20,7 +20,7 @@ public class GerenciaTarefa {
         descricaoAux = sc.nextLine();
         tarefas.add(new Tarefa(idAux, tituloAux, descricaoAux));
         System.out.println("Tarefa criada com sucesso!");
-        sc.next();
+        sc.nextLine();
     }
 
     //Método para listar tarefas
@@ -31,8 +31,27 @@ public class GerenciaTarefa {
             System.out.println("Listando tarefas!");
             for (int i = 0; i < tarefas.size(); i++){
                 System.out.println(i+1 + ". " + tarefas.get(i).getTitulo());
-                sc.next();
+            }
+            System.out.println("Escolha uma tarefa para ver os detalhes ou digite 0 para voltar ao menu.");
+            int op = sc.nextInt();
+            if (op == 0){
+
+            }else if (op < 0){ 
+                System.out.println("Número negativo - invalido!");
+            }else{
+                exibeTarefa(op);
             }
         }
+        sc.nextLine();
+    }
+
+    //Método para exibir informações detalhadas de uma tarefa
+    public void exibeTarefa(int op){
+        System.out.println("Id: " + tarefas.get(op-1).getId());
+        System.out.println("Titulo: " + tarefas.get(op-1).getTitulo());
+        System.out.println("Descrição: " + tarefas.get(op-1).getDescricao());
+        System.out.println("Data de Criação: " + tarefas.get(op-1).getDataFormatada());
+        System.out.println("Concluída: " + tarefas.get(op-1).isConcluida());
+        sc.nextLine();
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -7,13 +7,13 @@ public class Main {
         int opcao = 0;
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Bem vindo ao Gerenciador de Tarefas!\n\n");
+        System.out.println("Bem vindo ao Gerenciador de Tarefas!");
         
         do{
-        System.out.println("1. Criar tarefa.\n");
-        System.out.println("2. Listar tarefas.\n");
-        System.out.println("3. Editar tarefa.\n");
-        System.out.println("4. Remover tarefa.\n");
+        System.out.println("1. Criar tarefa.");
+        System.out.println("2. Listar tarefas.");
+        System.out.println("3. Editar tarefa.");
+        System.out.println("4. Remover tarefa.");
         System.out.println("5. Sair do programa.\n");
         opcao = sc.nextInt();
         
