5d61cd1853d3f6716d9a75295e44542fbbfdbd01
hugolca <hugolcabreu@gmail.com>
2025-11-20 00:17:05 -0300

    Implementação do método listaTarefas

--- src/GerenciaTarefa.java
+++ src/GerenciaTarefa.java
@@ -8,15 +8,31 @@ public class GerenciaTarefa {
     String tituloAux;
     String descricaoAux;
 
-
-    public void CriaTarefa(){
-        System.out.println("Id: ");
+    //Método para criar tarefa
+    public void criaTarefa(){
+        System.out.println("Criando tarefa. Insira os dados:");
+        System.out.print("Id: ");
         idAux = sc.nextInt();
-        System.out.println("Titulo: ");
+        sc.nextLine();
+        System.out.print("Titulo: ");
         tituloAux = sc.nextLine();
-        System.out.println("Descrição: ");
+        System.out.print("Descrição: ");
         descricaoAux = sc.nextLine();
-        
         tarefas.add(new Tarefa(idAux, tituloAux, descricaoAux));
+        System.out.println("Tarefa criada com sucesso!");
+        sc.next();
+    }
+
+    //Método para listar tarefas
+    public void listaTarefas(){
+        if (tarefas.size() == 0){
+            System.out.println("Crie uma tarefa primeiro!");
+        }else{
+            System.out.println("Listando tarefas!");
+            for (int i = 0; i < tarefas.size(); i++){
+                System.out.println(i+1 + ". " + tarefas.get(i).getTitulo());
+                sc.next();
+            }
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,31 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
+
+        GerenciaTarefa gerenciador = new GerenciaTarefa();
+        int opcao = 0;
         Scanner sc = new Scanner(System.in);
+
         System.out.println("Bem vindo ao Gerenciador de Tarefas!\n\n");
+        
+        do{
         System.out.println("1. Criar tarefa.\n");
         System.out.println("2. Listar tarefas.\n");
         System.out.println("3. Editar tarefa.\n");
         System.out.println("4. Remover tarefa.\n");
-        int opcao = sc.nextInt();
+        System.out.println("5. Sair do programa.\n");
+        opcao = sc.nextInt();
+        
+        switch(opcao){
 
-
-
+            case 1:
+                gerenciador.criaTarefa();
+                break;
+            case 2:
+                gerenciador.listaTarefas();
+                break;
+        }
+        }while(opcao!=5);
+        sc.close();
     }
-    
 }
